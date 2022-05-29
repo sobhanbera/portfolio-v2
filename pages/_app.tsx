3d511b6611c5741078532662427fc477bec3abda
Sobhan Bera <sobhanbera258@gmail.com>
2022-05-29 19:25:36 +0530

    followings: 1. modules added like marquee and aos 2. animation setup init. 3. some header bugs removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import type {AppProps} from 'next/app'
+import {useEffect} from 'react'
+
+import aos from 'aos'
 
 import '../styles/globals.scss'
 
 function MyApp({Component, pageProps}: AppProps) {
+    useEffect(() => {
+        aos.init({
+            duration: 3000,
+            initClassName: 'sb-initial-anim',
+            once: false,
+            mirror: true,
+            debounceDelay: 100,
+            throttleDelay: 100,
+        })
+    }, [])
+
     return <Component {...pageProps} />
 }
 
