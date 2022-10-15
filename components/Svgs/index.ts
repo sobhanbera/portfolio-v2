01bf285045a921feec6b00f1624d1978ddeba4d3
sobhanbera <sobhanbera258@gmail.com>
2022-10-16 00:36:31 +0530

    more svg icons

---
+++ components/Svgs/index.ts
@@ -0,0 +1,8 @@
+import EmailSvg from './Email'
+import GitHubSvg from './GitHub'
+import InstagramSvg from './Instagram'
+import LinkedinSvg from './Linkedin'
+import TwitterSvg from './Twitter'
+import WebsiteSvg from './Website'
+
+export {EmailSvg, GitHubSvg, InstagramSvg, LinkedinSvg, TwitterSvg, WebsiteSvg}
--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,29 @@ import InfiniteLoopingScrollView from './InfiniteLoopingScrollView'
 
 import Header from './Header'
 import Footer from './Footer'
+import FloatingFooter from './FloatingFooter'
 
 import Greeting from './Greeting'
 import Name from './Name'
 
 import CursorLover from './CursorLover'
 
-export {InfiniteLoopingScrollView, Header, Footer, Greeting, Name, CursorLover}
+import {
+    EmailSvg,
+    GitHubSvg,
+    InstagramSvg,
+    LinkedinSvg,
+    TwitterSvg,
+    WebsiteSvg,
+} from './Svgs'
+
+export {
+    InfiniteLoopingScrollView,
+    Header,
+    Footer,
+    FloatingFooter,
+    Greeting,
+    Name,
+    CursorLover,
+}
+export {EmailSvg, GitHubSvg, InstagramSvg, LinkedinSvg, TwitterSvg, WebsiteSvg}
