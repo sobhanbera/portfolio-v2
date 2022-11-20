50913adcd5e2c8cb54e129719800d7033cb33162
sobhanbera <sobhanbera258@gmail.com>
2022-11-20 23:45:58 +0530

    values updated

--- constants/index.ts
+++ constants/index.ts
@@ -4,12 +4,12 @@ export const MAX_HEADER_HEIGHT = 100 // the maximum height of the header
 export const ANIMATION_FACTOR = 0.4 // the factor by which each and every animation should be performed
 export const DEFAULT_ANIMATION_DURATION = 0.4 // default time for each animation in seconds
 
-export const HOME_URL_OR_TAG = '#home' // nothing more than the actual route
-export const ABOUT_ME_TAG = '#aboutme' // tag to move to different section in the page
-export const MY_BLOGS_TAG = '#blogs' // tag to move to different section in the page
-export const MY_EXPERIENCE_TAG = '#experience' // tag to move to different section in the page
-export const MY_PROJECTS_TAG = '#projects' // tag to move to different section in the page
-export const CONTACT_ME_TAG = '#contactme' // tag to move to different section in the page
+export const HOME_URL_OR_TAG = '/#home' // nothing more than the actual route
+export const ABOUT_ME_TAG = '/#aboutme' // tag to move to different section in the page
+export const MY_BLOGS_TAG = '/#blogs' // tag to move to different section in the page
+export const MY_EXPERIENCE_TAG = '/#experience' // tag to move to different section in the page
+export const MY_PROJECTS_TAG = '/#projects' // tag to move to different section in the page
+export const CONTACT_ME_TAG = '/#contactme' // tag to move to different section in the page
 
 export const HOME_URL_OR_STRING = 'home' // nothing more than the actual route
 export const ABOUT_ME_STRING = 'aboutme' // string for the same above
@@ -23,8 +23,6 @@ export const GITHUB_URL = 'https://github.com/sobhanbera' // link to my GitHub p
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/sobhanbera' // url of my linkedin profile
 export const INSTAGRAM_URL = 'https://www.instagram.com/sobhanbera_' // link of instagram account
 export const OLD_PORTFOLIO_URL = 'https://sobhanbera.github.io/oldportfolio' // the url of my old portfolio
-
-export const BLOGGING_WEBSITE = 'https://sobhanblogs.vercel.app/' // url of the blogging website
 
 export const COPYRIGHT_TEXT = '© 2022 Sobhan Bera'
 
