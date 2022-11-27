b87cce0b8838ca713826e25a7820a58d378edb4c
sobhanbera <sobhanbera258@gmail.com>
2022-11-28 04:31:26 +0530

    resume updated

--- constants/index.ts
+++ constants/index.ts
@@ -35,4 +35,4 @@ export const EMAIL_REGEX =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 // relative link to the resume file
-export const RESUME_LOCAL_URL = "/files/Sobhan Bera's Resume.pdf"
+export const RESUME_LOCAL_URL = "/files/Sobhan Bera's CV.pdf"
