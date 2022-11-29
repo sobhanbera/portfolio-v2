7c29ce0449fc16758d20b77dfa7794ffbd03b836
sobhanbera <sobhanbera258@gmail.com>
2022-11-30 02:36:50 +0530

    simple issue of fs module not found in the frontend

--- configs/env.ts
+++ configs/env.ts
@@ -1,8 +1,5 @@
 import {config} from 'dotenv'
 config()
 
-const {GA_TRACKING_ID_ENV} = process.env
-export {GA_TRACKING_ID_ENV}
-
 const {EMAIL, EMAIL_PASSWORD} = process.env
 export {EMAIL, EMAIL_PASSWORD}
