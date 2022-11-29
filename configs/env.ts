ae32aeee993dddca8af29901141a770513fc232a
sobhanbera <sobhanbera258@gmail.com>
2022-11-30 02:29:15 +0530

    feat: google analytics are enabled in the portfolio website now

--- configs/env.ts
+++ configs/env.ts
@@ -1,8 +1,8 @@
 import {config} from 'dotenv'
 config()
 
-const {GA_TRACKING_ID} = process.env
-export {GA_TRACKING_ID}
+const {GA_TRACKING_ID_ENV} = process.env
+export {GA_TRACKING_ID_ENV}
 
 const {EMAIL, EMAIL_PASSWORD} = process.env
 export {EMAIL, EMAIL_PASSWORD}
